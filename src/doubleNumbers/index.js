2bf6859ac829b72d735b13ad0330b726a84fe9c8
Vitaliy Vanchak <vvanchak2@gmail.com>
2018-10-29 17:43:19 +0200

    task 'doubleNumbers' finished

--- src/doubleNumbers/index.js
+++ src/doubleNumbers/index.js
@@ -1,4 +1,12 @@
 /* eslint-disable-next-line */
 export const doubleNum = num => {
-  // your logic here...
+  const stringNum = num.toString();
+  const numLength = stringNum.length;
+  let result = num * 2;
+  if ( numLength >= 2 && numLength % 2 === 0 ){
+    const firstPart = stringNum.slice( 0, numLength / 2 );
+    const secondPart = stringNum.slice( numLength / 2, numLength );
+    result = ( firstPart === secondPart ) ? num : num * 2;
+  }
+  return result;
 };
