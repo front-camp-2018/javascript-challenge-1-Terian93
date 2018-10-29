5931badb1068a349cefc83f896224313b50a43dd
Vitaliy Vanchak <vvanchak2@gmail.com>
2018-10-29 18:35:58 +0200

    task 'sortByWeight' finished

--- src/sortByWeight/index.js
+++ src/sortByWeight/index.js
@@ -1,4 +1,34 @@
 /* eslint-disable-next-line */
 export const sortByWeight = arr => {
-  // your logic here...
+
+  const arrWithPriority = []; 
+  const result = [];
+
+  arr.forEach( element => {
+    let priorityNumber = 0;
+    const elementSplited = element.split('');
+    elementSplited.forEach( element => {
+      priorityNumber += Number(element);
+    });
+    arrWithPriority.push({
+      'priority': priorityNumber,
+      'value': element
+    });
+  });
+  
+  arrWithPriority.sort( (a, b) => {
+    let comparison = 0;
+    if (a.priority > b.priority) {
+      comparison = 1;
+    } else if (a.priority < b.priority) {
+      comparison = -1;
+    }
+    return comparison;
+  });
+
+  for (let index = 0; index < arr.length; index++) {
+    result.push( arrWithPriority[index].value );
+  }
+
+  return result;
 };
